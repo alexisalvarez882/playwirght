1bec89560d716ca523f947df71ef1458b654d2b4
Alexis Alvarez <alexisalvarez@192.168.1.6>
2025-06-07 11:54:22 -0300

    signup tests

---
+++ tests/registro.spec.ts
@@ -0,0 +1,34 @@
+import { test, expect } from '@playwright/test';
+
+test('TC-1 Verify visual elements on sign up page', async ({ page }) => {
+  await page.goto('http://localhost:3000/');
+  await expect(page.locator('input[name="firstName"]')).toBeVisible();
+  await expect(page.locator('input[name="lastName"]')).toBeVisible();
+  await expect(page.locator('input[name="email"]')).toBeVisible();
+  await expect(page.locator('input[name="password"]')).toBeVisible();
+  await expect(page.getByTestId('boton-registrarse')).toBeVisible();  // Expect a title "to contain" a substring.
+  //await expect(page).toHaveTitle(/Playwright/);
+});
+
+test('TC-2 Verificar Boton de registro esta inhabilitado por defecto', async ({ page }) => {
+  await page.goto('http://localhost:3000/');
+  await expect(page.getByTestId('boton-registrarse')).toBeDisabled();
+});
+
+test('TC-3 Verificar que el botón de registro se habilita al completar los campos obligatorios', async ({ page }) => {
+  await page.goto('http://localhost:3000/');
+  await page.locator('input[name="firstName"]').fill('Alexis')
+  await page.locator('input[name="lastName"]').fill('Alvarez');
+  await page.locator('input[name="email"]').fill('alexis'+ Date.now().toString()+'@email.com');
+  await page.locator('input[name="password"]').fill('123456');
+  await expect(page.getByTestId('boton-registrarse')).toBeEnabled();
+});
+
+test('TC-4 Verificar redireccionamiento a página de inicio de sesión al hacer clic en el botón de registro', async ({ page }) => {
+  await page.goto('http://localhost:3000/');
+  await page.getByTestId('boton-login-header-signup').click();
+  await expect(page).toHaveURL('http://localhost:3000/login');
+});
+
+
+
